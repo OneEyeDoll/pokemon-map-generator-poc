10cf255f6c813aaccb4232717e4a7b62d3766a6f
oneeyedoll <efallletti@gmail.com>
2022-12-28 12:59:43 +0100

    initial

---
+++ mapparser.py
@@ -0,0 +1,56 @@
+from sys import argv
+
+import numpy as np
+import json
+
+offsets = []
+
+romfile = open("rom.gb","rb") 
+
+romfile.seek(0x01AE, 0)
+pointer_bytes = np.array_split(list(romfile.read(494)),247)
+
+romfile.seek(0xC23D, 0)
+banks_bytes = np.array_split(list(romfile.read(247)), 247)
+
+pointers = list(map(lambda val: int.from_bytes(list(val),"little") % 0x4000, pointer_bytes))
+banks = list(map(lambda val: int.from_bytes(list(val),"little"), banks_bytes))
+
+banks_map = {}
+
+width = 0
+height = 0
+pointer_map = []
+
+for i in range(0, 247):
+    offset = pointers[i] + 0x4000 * banks[i]
+    offsets.append(offset)
+    banks_map[offset] = banks[i]
+print(banks_map)
+
+for offset in offsets:
+    if(offset != 0x45CE5 and offset != 0x56B2 and offset != 0x762A2 and offset != 0x49A4 and offset != 0x5704):
+        romfile.seek(offset, 0)
+        tileset = int.from_bytes(list(romfile.read(1)),"little")
+        height = int.from_bytes(list(romfile.read(1)),"little")
+        width = int.from_bytes(list(romfile.read(1)),"little")
+        pointer_map = int.from_bytes(list(romfile.read(2)),"little")
+        abs_pointer_map = pointer_map + 0x4000 * banks_map[offset]
+
+        if(abs_pointer_map != 0):
+            datafile = open('out/'+str(hex(offset))+'.json','w')
+            romfile.seek(abs_pointer_map, 0)
+            map_data = list(romfile.read(width * height))
+            data = {
+                "width":width,
+                "height":height,
+                "map_data":map_data,
+                "tileset":tileset
+            }
+
+            datafile.write(json.dumps(data))
+            datafile.close()
+
+romfile.close()
+
+
---
+++ model.py
@@ -0,0 +1,223 @@
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+from tensorflow.keras import layers
+import time
+
+import tensorflow as tf
+
+import os
+import json
+
+
+BUFFER_SIZE = 60000
+BATCH_SIZE = 256
+
+EPOCHS = 50
+noise_dim = 100
+num_examples_to_generate = 16
+
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+
+def make_generator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(20*20*100, use_bias=False, input_shape=(100,)))
+    model.add(layers.BatchNormalization())
+    #model.add(layers.LeakyReLU())
+
+    model.add(layers.Reshape((20, 20, 100)))
+    assert model.output_shape == (None, 20, 20, 100)  # Note: None is the batch size
+
+    model.add(layers.Conv2DTranspose(50, (20, 20), padding='same', use_bias=False))
+    print(model.output_shape)
+    assert model.output_shape == (None, 20, 20, 50)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(25, (20, 20), strides=(2, 2), padding='same', use_bias=False))
+    print(model.output_shape)
+    assert model.output_shape == (None, 40, 40, 25)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(1, (20, 20), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    print(model.output_shape)
+    assert model.output_shape == (None, 80, 80, 1)
+
+    return model
+
+def make_discriminator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(25, (20, 20), strides=(2, 2), padding='same',input_shape=(80,80,1)) )
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.5))
+
+    model.add(layers.Conv2D(50, (20, 20), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.5))
+
+    model.add(layers.Conv2D(100, (20, 20), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.5))
+
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+
+    return model
+
+
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+# Notice the use of `tf.function`
+# This annotation causes the function to be "compiled".
+@tf.function
+def train_step(images):
+    noise = tf.random.normal([BATCH_SIZE, noise_dim], mean=80,stddev=100)
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+      generated_images = generator(noise, training=True)
+
+      real_output = discriminator(images, training=True)
+      fake_output = discriminator(generated_images, training=True)
+
+      gen_loss = generator_loss(fake_output)
+      disc_loss = discriminator_loss(real_output, fake_output)
+
+    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+def train(dataset, epochs):
+    for epoch in range(epochs):
+        start = time.time()
+
+        for image_batch in dataset:
+            train_step(image_batch)
+
+        # Produce images for the GIF as you go
+        #display.clear_output(wait=True)
+        generate_and_save_images(generator,
+                                epoch + 1,
+                                seed)
+
+        # Save the model every 15 epochs
+        #if (epoch + 1) % 15 == 0:
+        #checkpoint.save(file_prefix = checkpoint_prefix)
+
+        print('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+
+    # Generate after the final epoch
+    generate_and_save_images(generator,
+                            'final',
+                            seed)
+
+def generate_and_save_images(model, epoch, test_input):
+    # Notice `training` is set to False.
+    # This is so all layers run in inference mode (batchnorm).
+    predictions = model(test_input, training=False)
+    out = open('tfout/'+str(epoch)+'.json','w')
+    out.write(json.dumps(predictions.numpy().tolist()))
+
+
+map_matrix = {
+    "0":[],
+    "1":[],
+    "2":[],
+    "3":[],
+    "4":[],
+    "5":[],
+    "6":[],
+    "7":[],
+    "8":[],
+    "9":[],
+    "10":[],
+    "11":[],
+    "12":[],
+    "13":[],
+    "14":[],
+    "15":[],
+    "16":[],
+    "17":[],
+    "18":[],
+    "19":[],
+    "20":[],
+    "21":[],
+    "22":[],
+    "23":[]
+}
+paths = list(os.scandir("./out"))
+
+for j in range(len(paths)):
+    if paths[j].is_file():
+        file = open('out/'+paths[j].name, 'r')
+        data = json.loads(file.readline())
+        tileset = str(data['tileset'])
+        map_matrix[tileset].append([])
+        pos = len(map_matrix[tileset]) - 1
+        for i in range(0,data['height']):
+            map_matrix[tileset][pos].append([])
+            for k in range(0,data['width']):
+                map_matrix[tileset][pos][i].append([data['map_data'][i*data['width']+k]])
+
+
+generator = make_generator_model()
+
+noise = tf.random.normal([1, 100])
+generated_map = generator(noise, training=False)
+
+
+discriminator = make_discriminator_model()
+decision = discriminator(generated_map)
+print (decision)
+
+
+seed = tf.random.normal([num_examples_to_generate, noise_dim], mean=80,stddev=100)
+print(seed.shape)
+for i in range(len(map_matrix['0'])):
+    if len(map_matrix['0'][i]) < 80:
+        while len(map_matrix['0'][i]) < 80:
+            map_matrix['0'][i].append([[-1]] * 80)
+    for k in range(len(map_matrix['0'][i])):
+        if(len(map_matrix['0'][i][k]) < 80):
+            while len(map_matrix['0'][i][k]) < 80:
+                map_matrix['0'][i][k] += ([[-1]] * (80 - len(map_matrix['0'][i][k])))
+
+
+np_matrix_tile0 = np.asarray(map_matrix['0']).astype('float32')
+np_matrix_tile0 = np_matrix_tile0.reshape((-1,34,80,80,1))
+print(np_matrix_tile0.shape)
+#print(np_matrix_tile0.tolist()[0][0])
+train(np_matrix_tile0, EPOCHS)
+
+
+"""
+model = keras.Sequential()
+
+# Add an Embedding layer expecting input vocab of size 1000, and
+# output embedding dimension of size 64.
+model.add(layers.Embedding(input_dim=1000, output_dim=64))
+
+# Add a LSTM layer with 128 internal units.
+model.add(layers.LSTM(128))
+
+# Add a Dense layer with 10 units.
+model.add(layers.Dense(10))
+
+model.summary()
+"""
